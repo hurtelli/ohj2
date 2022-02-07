00990e5c34df921dcf1d72c0d362f70d0963c21f
Otto Palmgren <otto.palmgren@tuni.fi>
2022-02-07 18:03:09 +0200

    zipboing valmis

--- templates/02/number_series_game/main.cpp
+++ templates/02/number_series_game/main.cpp
@@ -3,7 +3,38 @@
 
 int main()
 {
+    int amount = 0;
     std::cout << "How many numbers would you like to have? ";
+    std::cin >> amount;
+
+    int i = 1;
+    while (i < amount + 1)
+    {
+
+                if(i % 3 == 0 and i % 7 != 0)
+        {
+               std::cout << "zip" << std::endl;
+
+        }
+
+               else if(i % 7 == 0 and i % 3 != 0)
+         {
+               std::cout << "boing" << std::endl;
+
+         }
+                else if(i % 7 == 0 and i % 3 == 0)
+         {
+                    std::cout << "zip boing" << std::endl;
+         }
+
+                else
+         {
+                std::cout << i << std::endl;
+
+         }
+                ++i;
+    }
+
 
     return 0;
 }
