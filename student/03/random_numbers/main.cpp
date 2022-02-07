f11743d456e2efe9b2793d31e3ae3b76483448b8
Otto Palmgren <otto.palmgren@tuni.fi>
2022-02-07 19:43:26 +0200

    olispa näihin ees vittu ohjeet

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -4,18 +4,35 @@
 
 using namespace std;
 
-void produce_random_numbers(unsigned int lower, unsigned int upper)
+void produce_random_numbers(unsigned int lower, unsigned int upper, unsigned int seed)
 {
-    // Implement your function here
+    int i = 0;
+    char input = 'o';
+    while(i == 0)
+    {
+    default_random_engine gen(seed);
+    uniform_int_distribution<int> distr(lower, upper);
+    cout << "Your drawn random number is " << distr(gen)<< endl;
+
+
+    cout << "Press q to quit or any other key to continue: ";
+    cin >> input;
+    if (input == 'q')
+    {
+        return;
+    }
+    }
 }
 
 int main()
 {
-    unsigned int lower_bound, upper_bound;
+    unsigned int lower_bound, upper_bound, seed;
     cout << "Enter a lower bound: ";
     cin >> lower_bound;
     cout << "Enter an upper bound: ";
     cin >> upper_bound;
+    cout << "Enter a seed value: ";
+    cin >> seed;
 
     if(lower_bound >= upper_bound)
     {
@@ -24,7 +41,7 @@ int main()
         return EXIT_FAILURE;
     }
 
-    produce_random_numbers(lower_bound, upper_bound);
+    produce_random_numbers(lower_bound, upper_bound, seed);
 
     return EXIT_SUCCESS;
 }
