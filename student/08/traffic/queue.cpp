c7b5e6419507813553664746763639e8834bb6c4
Otto Palmgren <otto.palmgren@tuni.fi>
2022-03-23 16:35:23 +0200

    jaahans

--- student/08/traffic/queue.cpp
+++ student/08/traffic/queue.cpp
@@ -8,11 +8,17 @@ Queue::Queue(const unsigned int c)
 
 Queue::~Queue()
 {
-
+    Vehicle* current = first_;
+    while(current){
+        Vehicle* removable = current;
+        current = current->next;
+        delete removable;
+    }
 }
 
 void Queue::enqueue(const string &reg)
 {
+
     if(!is_green_){
         Vehicle* newid = new Vehicle;
         newid->reg_num = reg;
@@ -20,10 +26,8 @@ void Queue::enqueue(const string &reg)
 
         //jos que on tyhjä
         if(first_ == nullptr){
-            std::cout<<"tyhjä que"<<std::endl;
             first_ = newid;
             last_ = newid;
-            std::cout<<first_->reg_num<<std::endl;
             return;
         }
 
@@ -37,37 +41,52 @@ void Queue::enqueue(const string &reg)
     }
 }
 
+void Queue::dequeue()
+{
+    if(!first_){
+        return;
+    }
+    //jos on seuraava
+    if(first_->next){
+        Vehicle* removable = first_;
+        first_ = removable->next;
+        //poistetaan poistettava
+        delete removable;
+        return;
+    }else{ //jos ei ole seuraavaa autoa
+        Vehicle* lonely = first_;
+        delete lonely;
+        first_ = nullptr;
+        last_ = nullptr;
+    }
+
+}
+
 void Queue::switch_light()
 {
     //jos jonossa on auto
     if(first_){
         std::cout<<"GREEN: Vehicle(s) ";
         Vehicle* current = first_;
-        unsigned int passed=0;
-        while(current and current->next){
-            //jos 3 autoa ei ole mennyt vielä
-            if(passed<cycle_){
-                //poistettava
-                Vehicle* removable = current;
-                std::cout<<removable->reg_num<<" ";
-                //currentti sueraavaan
+        unsigned int passed = 0;
+        //löytyy seuraava jonosta ja passattu alle 3
+        while(current and current->next and passed<cycle_){
+            Vehicle* remove = current;
+            std::cout<<remove->reg_num<<" ";
+            current = remove->next;
+            dequeue();
+            ++passed;
+        }
+        //jos ei ole sueraavaa
+        if(!current->next and passed<cycle_){
+            std::cout<<current->reg_num<<" ";
+            dequeue();
+            passed++;
+        }
 
-                current = current->next;
-                //poistetaan poistettava
-                delete removable;
-            }
-            first_ = current;
-        }
-        if(passed<cycle_){
-            Vehicle* removable = current;
-            std::cout<<removable->reg_num<<" ";
-            delete removable;
-            first_ = nullptr;
-            last_ = nullptr;
-        }
-        is_green_ = false;
-        std::cout<<"can go on";
+        std::cout<<"can go on"<<std::endl;
     }
+
     //jos jonossa ei ole autoja pelkästään valokytkin toiminta
     else{
         if(is_green_){
@@ -84,6 +103,8 @@ void Queue::switch_light()
 void Queue::reset_cycle(unsigned int cycle)
 {
     cycle_ =cycle;
+    is_green_ = true;
+
 }
 
 void Queue::print() const
